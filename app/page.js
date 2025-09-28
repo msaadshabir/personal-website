cbfbe0b5ad983d05029e36c28295dc2135658d5c
saad <saad.shabir@hotmail.com>
2025-09-28 18:02:56 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -17,8 +17,8 @@ export default function Home() {
           <div>
             <p className="font-medium">AriesTECH</p>
             <div className="ml-4 mt-0.5 space-y-0.5 font-normal">
-              <p>• Installed, configured, and maintained Cisco routers & switches; designed Layer 2/3 segmentation (VLANs, trunking, inter-VLAN routing) and implemented static, RIP, and OSPF routing to optimize traffic flow and improve security across the LAN/WAN.</p>
-              <p>• Ensured ~99% network availability via proactive monitoring, rapid diagnostics (using packet-analysis / IOS tools), and regular preventative maintenance (firmware upgrades, ACL reviews, configuration backups).</p>
+              <p>● Installed, configured, and maintained Cisco routers & switches; designed Layer 2/3 segmentation (VLANs, trunking, inter-VLAN routing) and implemented static, RIP, and OSPF routing to optimize traffic flow and improve security across the LAN/WAN.</p>
+              <p>● Ensured ~99% network availability via proactive monitoring, rapid diagnostics (using packet-analysis / IOS tools), and regular preventative maintenance (firmware upgrades, ACL reviews, configuration backups).</p>
             </div>
           </div>
         </div>
@@ -48,7 +48,7 @@ export default function Home() {
 
         <h3 className="text-xl font-semibold mb-1 text-black">Get in touch:</h3>
         <div className="flex items-center space-x-4 mb-1">
-          <a href="mailto:saad.shabir@hotmail.com" className="text-gray-900 font-normal">saad.shabir@hotmail.com</a>
+          <a href="mailto:saad.shabir@hotmail.com" className="text-gray-900 font-normal underline">saad.shabir@hotmail.com</a>
           <div className="flex items-center space-x-4">
             <a href="https://www.linkedin.com/in/saadshabir/" target="_blank" rel="noopener noreferrer">
               <svg aria-label="LinkedIn" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin text-gray-900 hover:text-gray-600"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect width="4" height="12" x="2" y="9"></rect><circle cx="4" cy="4" r="2"></circle></svg>
