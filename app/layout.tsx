3dec7f7748ddd9ea34c2d6a22a9d352d230050b7
saad <saad.shabir@hotmail.com>
2025-11-23 01:42:18 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,7 +80,8 @@ export const metadata: Metadata = {
 export const viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
+  maximumScale: 5,
+  userScalable: true,
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "#fafafa" },
   ],
